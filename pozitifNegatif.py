f341a532bfc1b1e78d4cdb68a26d167c21ae53e5
tecnymans2 <technymans2yazilim@gmail.com>
2022-12-10 11:27:29 +0300

    Create pozitifNegatif.py

---
+++ pozitifNegatif.py
@@ -0,0 +1,13 @@
+#@technymans / pozitif veya nefatif olduğuna bakalım.
+
+sayi = int(input("sayı giriniz."))
+
+if sayi == 0 :
+    print("sıfırdır")
+    
+elif sayi > 0 :
+    print("pozitif")
+    
+else  :
+    print("negatif")
+    
