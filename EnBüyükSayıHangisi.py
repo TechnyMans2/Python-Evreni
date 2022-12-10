d6172d462476a515cef4d3e89d6b619a607306ce
tecnymans2 <technymans2yazilim@gmail.com>
2022-12-10 11:27:16 +0300

    Create EnBüyükSayıHangisi.py

---
+++ EnBüyükSayıHangisi.py
@@ -0,0 +1,15 @@
+#@technymans / en büyük sayıyı bulduk.
+
+sayi1 = int(input("sayı1 giriniz."))
+sayi2 = int(input("sayı2 giriniz."))
+sayi3 = int(input("sayı3 giriniz."))
+ 
+if (sayi1>=sayi2) and (sayi1>=sayi3):
+    print("sayi1 büyüktür")
+    
+elif (sayi2>=sayi1) and (sayi2>=sayi3):
+    print("sayi2 büyüktür")
+    
+else :
+    print("sayi3 büyüktür")
+    
